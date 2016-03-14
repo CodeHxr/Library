170dd2a459605d59e7aacae943ba5ae369e71932
Joseph Hicks <captainn64@gmail.com>
2016-03-14 10:50:06 -0500

    Added initial classes

---
+++ jCrypto/KeyPair.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace jCrypto
+{
+    public class KeyPair
+    {
+        public string Public { get; set; }
+        public string Private { get; set; }
+    }
+}
---
+++ jCrypto/PublicKey.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace jCrypto
+{
+    public class PublicKey
+    {
+        public static KeyPair GenerateKeyPair()
+        {
+            var rsa = new RSACryptoServiceProvider();
+            var publicKey = rsa.ToXmlString(false);
+            var privateKey = rsa.ToXmlString(true);
+
+            var pair = new KeyPair {Private = privateKey, Public = publicKey};
+            return pair;
+        }
+    }
+}
