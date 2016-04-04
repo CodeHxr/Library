15f7edb686668c7940a08ebf2d8e1ba05ded4bed
Joseph Hicks <captainn64@gmail.com>
2016-04-04 16:48:51 -0500

    updates to sprite object - more enhancements to come

--- jSpriteEngine/Sprite.cs
+++ jSpriteEngine/Sprite.cs
@@ -2,41 +2,52 @@
 using System.Collections.Generic;
 using System.Drawing;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace jSpriteEngine
 {
     public class Sprite
     {
-        private readonly List<Image> _frames = new List<Image>();
-
-        public double SecondsPerFrame = 0.5;
+        private readonly List<RectangleF> _frames = new List<RectangleF>();
+        private readonly Image _sourceImage;
+        //public double FramesPerSecond { get; set; } = 1 / 30f; // 1 / [x] frames per second
         private DateTime _lastUpdateTime = DateTime.MinValue;
         public float Scale { get; set; } = 1.0f;
         public PointF Position { get; set; } = new PointF(0, 0);
 
-        public Sprite(Image sourceImage, Rectangle frameSize)
+        private double _framesPerSecond = 1 / 10f;
+        public int FramesPerSecond
+        {
+            get { return (int)(1 / _framesPerSecond); }
+            set { _framesPerSecond = 1f / value; }
+        }
+
+        public Sprite(Image sourceImage, Size frameSize)
         {
             for (var y = 0; y < sourceImage.Height; y += frameSize.Height)
             {
                 for (var x = 0; x < sourceImage.Width; x += frameSize.Width)
                 {
-                    var img = new Bitmap(sourceImage, frameSize.Width, frameSize.Height);
-                    _frames.Add(img);
+                    var frame = new RectangleF(x, y, frameSize.Width, frameSize.Height);
+                    _frames.Add(frame);
                 }
             }
+            _sourceImage = sourceImage;
         }
+
+        public Sprite(Image sourceImage, int framesAcross, int framesDown)
+            : this(sourceImage, new Size(sourceImage.Width / framesAcross, sourceImage.Height / framesDown))
+        { }
 
         public void Draw(Graphics g)
         {
             UpdateFrames();
 
-            var img = _frames.First();
-            var sourceRect = new RectangleF(0, 0, img.Width, img.Height);
-            var destRect = new RectangleF(Position, new SizeF(img.Width * Scale, img.Height * Scale));
+            //g.Clear(Color.Transparent);
 
-            g.DrawImage(_frames.First(), destRect, sourceRect, GraphicsUnit.Pixel);
+            var sourceRect = _frames.First();
+            var destRect = new RectangleF(Position, new SizeF(sourceRect.Width * Scale, sourceRect.Height * Scale));
+
+            g.DrawImage(_sourceImage, destRect, sourceRect, GraphicsUnit.Pixel);
         }
 
         private void UpdateFrames()
@@ -48,12 +59,12 @@ namespace jSpriteEngine
             }
 
             var timeSinceLastUpdate = (DateTime.Now - _lastUpdateTime).TotalSeconds;
-            if (timeSinceLastUpdate < SecondsPerFrame) return;
+            if (timeSinceLastUpdate < _framesPerSecond) return;
 
             _lastUpdateTime = DateTime.Now;
-            var img = _frames.First();
-            _frames.Remove(img);
-            _frames.Add(img);
+            var frame = _frames.First();
+            _frames.Remove(frame);
+            _frames.Add(frame);
         }
     }
 }
