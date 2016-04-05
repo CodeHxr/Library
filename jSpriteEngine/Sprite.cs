11e94f9ad265b0ff7ff9ccda0f43a6c86d4012b4
Joseph Hicks <captainn64@gmail.com>
2016-04-05 09:54:30 -0500

    added support for framesets

--- jSpriteEngine/Sprite.cs
+++ jSpriteEngine/Sprite.cs
@@ -7,20 +7,26 @@ namespace jSpriteEngine
 {
     public class Sprite
     {
+        #region Private Members
         private readonly List<RectangleF> _frames = new List<RectangleF>();
+        private readonly Dictionary<string, List<RectangleF>> _frameSets = new Dictionary<string, List<RectangleF>>(); 
         private readonly Image _sourceImage;
-        //public double FramesPerSecond { get; set; } = 1 / 30f; // 1 / [x] frames per second
+        private double _framesPerSecond = 1 / 10f;
         private DateTime _lastUpdateTime = DateTime.MinValue;
+        #endregion
+
+        #region Public Properties
         public float Scale { get; set; } = 1.0f;
         public PointF Position { get; set; } = new PointF(0, 0);
-
-        private double _framesPerSecond = 1 / 10f;
+        public string ActiveFrameset { get; set; } = "";
         public int FramesPerSecond
         {
             get { return (int)(1 / _framesPerSecond); }
             set { _framesPerSecond = 1f / value; }
         }
+        #endregion
 
+        #region Constructors
         public Sprite(Image sourceImage, Size frameSize)
         {
             for (var y = 0; y < sourceImage.Height; y += frameSize.Height)
@@ -38,18 +44,34 @@ namespace jSpriteEngine
             : this(sourceImage, new Size(sourceImage.Width / framesAcross, sourceImage.Height / framesDown))
         { }
 
+        public Sprite(Image sourceImage, int framesAcross, int framesDown, params SpriteFrameset[] framesets)
+            :this(sourceImage, framesAcross, framesDown)
+        {
+            foreach (var frameset in framesets)
+            {
+                var frames = frameset.FrameIndicies.Select(i => _frames[i]).ToList();
+                _frameSets.Add(frameset.Name, frames);
+            }
+        }
+        #endregion
+
+        #region Public Methods
         public void Draw(Graphics g)
         {
             UpdateFrames();
 
-            //g.Clear(Color.Transparent);
+            var frame = _frameSets.ContainsKey(ActiveFrameset) ? 
+                _frameSets[ActiveFrameset].First() : 
+                _frames.First();
 
-            var sourceRect = _frames.First();
+            var sourceRect = frame;
             var destRect = new RectangleF(Position, new SizeF(sourceRect.Width * Scale, sourceRect.Height * Scale));
 
             g.DrawImage(_sourceImage, destRect, sourceRect, GraphicsUnit.Pixel);
         }
+        #endregion
 
+        #region Private Methods
         private void UpdateFrames()
         {
             if (_lastUpdateTime == DateTime.MinValue)
@@ -61,10 +83,36 @@ namespace jSpriteEngine
             var timeSinceLastUpdate = (DateTime.Now - _lastUpdateTime).TotalSeconds;
             if (timeSinceLastUpdate < _framesPerSecond) return;
 
+            List<RectangleF> currentFrames;
+            if (string.IsNullOrWhiteSpace(ActiveFrameset) || _frameSets.ContainsKey(ActiveFrameset) == false)
+            {
+                // No active frameset - use full set
+                currentFrames = _frames;
+            }
+            else
+            {
+                currentFrames = _frameSets[ActiveFrameset];
+            }
+
             _lastUpdateTime = DateTime.Now;
-            var frame = _frames.First();
-            _frames.Remove(frame);
-            _frames.Add(frame);
+            var frame = currentFrames.First();
+            currentFrames.Remove(frame);
+            currentFrames.Add(frame);
+        }
+        #endregion
+    }
+
+    public class SpriteFrameset
+    {
+        public string Name { get; set; }
+        public List<int> FrameIndicies { get; set; }
+
+        public SpriteFrameset(){}
+
+        public SpriteFrameset(string name, params int[] frameIndicies)
+        {
+            Name = name;
+            FrameIndicies = new List<int>(frameIndicies);
         }
     }
 }
